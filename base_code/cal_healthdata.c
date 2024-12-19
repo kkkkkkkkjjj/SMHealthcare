ac1e1dfbc08c2cbaad8f31a9511a2d1e32f98a9d
kkkkkkkkjjj <144752086+kkkkkkkkjjj@users.noreply.github.com>
2024-12-19 14:10:39 +0900

    cal_healthdata.c : style
    줄간격, 들여쓰기 수정

--- base_code/cal_healthdata.c
+++ base_code/cal_healthdata.c
@@ -29,9 +29,9 @@
 */
 
 void saveData(const char* HEALTHFILEPATH, const HealthData* health_data) {
-	int i;
+    int i;
 	
-	//*(1) open the health_txt file 
+    //*(1) open the health_txt file 
     FILE* file = fopen(HEALTHFILEPATH, "w");
     
     if (file == NULL) {
@@ -41,25 +41,27 @@ void saveData(const char* HEALTHFILEPATH, const HealthData* health_data) {
 
     // ToCode: to save the chosen exercise and total calories burned 
     fprintf(file, "[Exercises] \n");
+	
     //*(1) write exercise history in "health_data.txt"
     for(i=0; i<health_data->exercise_count; i++)//exercise_count : number of exercises player has selected 
     {
-    fprintf(file, "%s",health_data->exercises[i].exercise_name);// write name
-    fprintf(file, "- %skcal\n",health_data->exercises[i].calories_burned_duration);	// write calories burned
-	}
+         fprintf(file, "%s",health_data->exercises[i].exercise_name);// write name
+         fprintf(file, "- %skcal\n",health_data->exercises[i].calories_burned_duration);// write calories burned
+    }
     //*(2) write total calories burned "health_data.txt"
     fprintf(file, "Total calories burned : %d", health_data->total_calories_burned);
     
     
-	// ToCode: to save the chosen diet and total calories intake 
+    // ToCode: to save the chosen diet and total calories intake 
     fprintf(file, "\n[Diets] \n");
+	
     //*(1) write diet history in "health_data.txt"
     for(i=0; i<health_data->diet_count; i++)//diet count : number of diets player has selected
     {
-    fprintf(file, "%s",health_data->diet[i].food_name);// write name 
-    fprintf(file, "%skcal\n",health_data->diet[i].calories_intake);//write calories
-	}
-	//*(2) write total calories intake in "health_data.txt"
+         fprintf(file, "%s",health_data->diet[i].food_name);// write name 
+         fprintf(file, "%skcal\n",health_data->diet[i].calories_intake);//write calories
+    }
+    //*(2) write total calories intake in "health_data.txt"
     fprintf(file, "Total calories intake : %d", health_data->total_calories_intake);
 
 
@@ -67,12 +69,11 @@ void saveData(const char* HEALTHFILEPATH, const HealthData* health_data) {
     // ToCode: to save the total remaining calrories
     fprintf(file, "\n[Total] \n");
     fprintf(file, "Basal metabolic rate - 1300 kcal\n");
+	
     //*(1) calculate the remaining calories and write in "health_data. txt"
     fprintf(file, "The remaining calories - %d kcal", (health_data->total_calories_intake-health_data->total_calories_burned-1300));
-    
+    //*(2) close the file
     fclose(file);
-    
-    
 }//(24.12.15 by ayoung cho)
 
 /*
@@ -86,42 +87,41 @@ void saveData(const char* HEALTHFILEPATH, const HealthData* health_data) {
 */
 
 void printHealthData(const HealthData* health_data) {
-	int i;
+     int i;
 	
-	// ToCode: to print out the saved history of exercises
-	printf("=========================== History of Exercise =======================\n");
-	//*(1) print every exercise history of the player
+    // ToCode: to print out the saved history of exercises
+     printf("=========================== History of Exercise =======================\n");
+     
+    //*(1) print every exercise history of the player
     for(i=0; i<(health_data->exercise_count); i++) 
     {
-        printf("Exercise : %s, Calories burned : %d kcal\n",health_data->exercises[i].exercise_name, health_data->exercises[i].calories_burned_duration); 
+           printf("Exercise : %s, Calories burned : %d kcal\n",health_data->exercises[i].exercise_name, health_data->exercises[i].calories_burned_duration); 
     }
-  
     printf("=======================================================================\n");
 
     // ToCode: to print out the saved history of diets
     printf("============================= History of Diet =========================\n");
     //*(2)print every diet history of the player
-	for(i=0; i< (health_data->diet_count); i++)
-	{
-       printf("Food : %s, Calories intake : %d kcal\n",health_data->diet[i].food_name, health_data->diet[i].calories_intake);
+    for(i=0; i< (health_data->diet_count); i++)
+    {
+           printf("Food : %s, Calories intake : %d kcal\n",health_data->diet[i].food_name, health_data->diet[i].calories_intake);
     }
-
     printf("=======================================================================\n");
 
-
-	// ToCode: to print out the saved history of calories including basal metabolic rate, 
-	// total calories burned, total calories intake, and the remaining calories
-	//*(1) bring the data from health_data struct
-	printf("============================== Total Calories =========================\n");
+    // ToCode: to print out the saved history of calories including basal metabolic rate
+	
+    // total calories burned, total calories intake, and the remaining calories
+    //*(1) bring the data from health_data struct
+    printf("============================== Total Calories =========================\n");
     printf("Basal Metabolic Rate : 1300 kcal\n");
-	printf("Total calories burned : %d kcal\n", health_data->total_calories_burned);
-	printf("Total calories intake : %d kcal\n", health_data->total_calories_intake);
-	printf("The remaining calories :%d kcal\n", (health_data->total_calories_intake-health_data->total_calories_burned-1300));
+    printf("Total calories burned : %d kcal\n", health_data->total_calories_burned);
+    printf("Total calories intake : %d kcal\n", health_data->total_calories_intake);
+    printf("The remaining calories :%d kcal\n", (health_data->total_calories_intake-health_data->total_calories_burned-1300));
     printf("=======================================================================\n \n");
     //(24.12.18. by ayoung cho) 
 	
-	// ToCode: to print out the recommendtaion depending on the current total calories burned and intake  
-	//*(1)REMAINING CAL = 0  
+    // ToCode: to print out the recommendtaion depending on the current total calories burned and intake  
+    //*(1)REMAINING CAL = 0  
     if((health_data->total_calories_intake-health_data->total_calories_burned-1300)==0)
     {
         printf("You have consumed all your calories for today!\n");
