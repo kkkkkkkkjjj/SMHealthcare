f5472e9b0c0363775532c3e68c1ecb0135ebdd6c
kkkkkkkkjjj <144752086+kkkkkkkkjjj@users.noreply.github.com>
2024-12-19 17:03:01 +0900

    final
    간격 및 들여쓰기 수정

--- base_code/cal_diets.c
+++ base_code/cal_diets.c
@@ -47,18 +47,19 @@ void loadDiets(const char* DIETFILEPATH) {
     }
     
    // ToCode: to read a list of the diets from the given file
-   //*(1) read "diets_txt" and save as {food_name, calories_ intake} form  in diet_list 
-   /// put break; condition fr. basecode.zip into while() by "&&"
-    while (diet_list_size < MAX_DIETS && 
-    fscanf(file, "%s %d", diet_list[diet_list_size].food_name,&diet_list[diet_list_size].calories_intake) == 2) 
-    {
-    //*(2) increase the number of diest_list (6 IS THE MAX NUM OF DIETS)    
-    diet_list_size++;
-    }
+     //*(1) read "diets_txt" and save as {food_name, calories_ intake} form  in diet_list 
+     /// put break; condition fr. basecode.zip into while() by "&&"
+      while (diet_list_size < MAX_DIETS && 
+         fscanf(file, "%s %d", diet_list[diet_list_size].food_name,&diet_list[diet_list_size].calories_intake) == 2) 
+         {
+     //*(2) increase the number of diest_list (6 IS THE MAX NUM OF DIETS)    
+            diet_list_size++;
+         }
     //*(3) close the file
-    fclose(file);
+         fclose(file);
     
 }//(24.12.16.by ayoung.cho)
+
 
 /*
     description : to enter the selected diet and the total calories intake in the health data
@@ -76,27 +77,30 @@ void inputDiet(HealthData* health_data) {
     int choice, i;
     
     // ToCode: to provide the options for the diets to be selected
-    printf("The list of diets:\n");
-    //*(1) print every diet in diet_list : { food_name, (calories_intake kcal) } form 
-    for(i=0; i<diet_list_size; i++)
-    {
-    printf("%d. %s (%d kcal)\n", i+1, diet_list[i].food_name, diet_list[i].calories_intake);
-    }
+      //*(1) print every diet in diet_list : { food_name, (calories_intake kcal) } form
+        printf("The list of diets:\n");
+        for(i=0; i<diet_list_size; i++)
+        {
+           printf("%d. %s (%d kcal)\n", i+1, diet_list[i].food_name, diet_list[i].calories_intake);
+        }
 
+	
     // ToCode: to enter the diet to be chosen with exit option
-    printf("Enter the diet : ");
-    scanf("%i", &choice);        
-    
+        printf("Enter the diet : ");
+        scanf("%i", &choice);        
+
+	
     // ToCode: to enter the selected diet in the health data
-	//*(1) enter the food name that player chose     
-    strcpy(health_data->diet[health_data->diet_count].food_name ,diet_list[choice-1].food_name);
-    
+      //*(1) enter the food name that player chose     
+        strcpy(health_data->diet[health_data->diet_count].food_name ,diet_list[choice-1].food_name);
+
+	
     // ToCode: to enter the total calories intake in the health data
-    //*(1)  enter the calories intake of the food that player chose
-    health_data->diet[health_data->diet_count].calories_intake = diet_list[choice-1].calories_intake;
-	//*(2) to add calories intake to total calories intake in health_data
-    health_data->total_calories_intake += diet_list[choice-1].calories_intake;
-	//*(3) to add up the number of diets player chose
+      //*(1)  enter the calories intake of the food that player chose
+        health_data->diet[health_data->diet_count].calories_intake = diet_list[choice-1].calories_intake;
+      //*(2) to add calories intake to total calories intake in health_data
+        health_data->total_calories_intake += diet_list[choice-1].calories_intake;
+      //*(3) to add up the number of diets player chose
 	health_data->diet_count++; 
 
 	    
