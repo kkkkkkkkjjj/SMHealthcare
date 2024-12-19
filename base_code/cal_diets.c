c845cfd2877acb9d5d8b0f40e5c32e5a8b7be2b6
kkkkkkkkjjj <144752086+kkkkkkkkjjj@users.noreply.github.com>
2024-12-19 13:55:57 +0900

    cal_diets.c : style
    - 줄간격 , 들여쓰기 조정

--- base_code/cal_diets.c
+++ base_code/cal_diets.c
@@ -45,14 +45,14 @@ void loadDiets(const char* DIETFILEPATH) {
         return;
     }
     
-	// ToCode: to read a list of the diets from the given file
-	//*(1) read "diets_txt" and save as {food_name, calories_ intake} form  in diet_list 
-	///    put break; condition fr. basecode.zip into while() by "&&"
+   // ToCode: to read a list of the diets from the given file
+   //*(1) read "diets_txt" and save as {food_name, calories_ intake} form  in diet_list 
+   /// put break; condition fr. basecode.zip into while() by "&&"
     while (diet_list_size < MAX_DIETS && 
-	fscanf(file, "%s %d", diet_list[diet_list_size].food_name,&diet_list[diet_list_size].calories_intake) == 2) 
+    fscanf(file, "%s %d", diet_list[diet_list_size].food_name,&diet_list[diet_list_size].calories_intake) == 2) 
     {
-	//*(2) increase the number of diest_list (6 IS THE MAX NUM OF DIETS)    
-	diet_list_size++;
+    //*(2) increase the number of diest_list (6 IS THE MAX NUM OF DIETS)    
+    diet_list_size++;
     }
     //*(3) close the file
     fclose(file);
@@ -77,22 +77,22 @@ void inputDiet(HealthData* health_data) {
     for(i=0; i<diet_list_size; i++)
     {
     printf("%d. %s (%d kcal)\n", i+1, diet_list[i].food_name, diet_list[i].calories_intake);
-	}
+    }
 
-	// ToCode: to enter the diet to be chosen with exit option
+    // ToCode: to enter the diet to be chosen with exit option
     printf("Enter the diet : ");
     scanf("%d", &choice);        
     
     // ToCode: to enter the selected diet in the health data
-	//*(1) enter the food name that player chose     
+    //*(1) enter the food name that player chose     
     strcpy(health_data->diet[health_data->diet_count+1].food_name ,diet_list[choice-1].food_name);
     
     // ToCode: to enter the total calories intake in the health data
     //*(1)  enter the calories intake of the food that player chose
     health_data->diet[health_data->diet_count+1].calories_intake = diet_list[choice-1].calories_intake;
-	//*(2) to add calories intake to total calories intake in health_data
+    //*(2) to add calories intake to total calories intake in health_data
     health_data->total_calories_intake += diet_list[choice-1].calories_intake;
-	//*(3) to count the number of diet player chose
-	health_data->diet_count++;
+    //*(3) to count the number of diet player chose
+    health_data->diet_count++;
 	    
 }//(24.12.18 by ayoungcho)
